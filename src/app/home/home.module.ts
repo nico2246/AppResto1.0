2b6cb5bade19c1c8d92b03393422e506644a105c
UentzY <jo.bonhomme@duocuc.cl>
2022-08-29 09:50:58 -0400

    Full commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { LoginPage } from './../login/login.page';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, LoginPage]
 })
 export class HomePageModule {}
